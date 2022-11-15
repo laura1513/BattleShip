213200ead73eb2e640a3cafa3024bb5ca626399f
laura1513 <laurajorques2003@gmail.com>
2022-11-15 18:58:30 +0100

    Ventana Principal

--- InicioBattle.java
+++ InicioBattle.java
@@ -2,14 +2,23 @@ import javax.swing.*;
 
 public class InicioBattle extends JFrame{
 
-    private JLabel Titulo;
-    private JButton EmpezarJuego;
+    private JPanel principal;
+    private JLabel titulo;
+    private JButton empezarJuego;
+    private JButton salirJuego;
 
     public InicioBattle() {
-
+        super("");
+        setContentPane(principal);
+        setSize(600,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
         InicioBattle app = new InicioBattle();
     }
+
+    
 }
