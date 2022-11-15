f840c8834f6d2299cc8c3967d7fb886619392355
laura1513 <laurajorques2003@gmail.com>
2022-11-15 17:45:18 +0100

    Initial commit

---
+++ InicioBattle.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class InicioBattle extends JFrame{
+
+    private JLabel Titulo;
+    private JButton EmpezarJuego;
+
+    public InicioBattle() {
+
+    }
+
+    public static void main(String[] args) {
+        InicioBattle app = new InicioBattle();
+    }
+}
