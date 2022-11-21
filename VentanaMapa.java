c119d5ce37db6aaa1a1b0c2ae166165a6396a33d
laura1513 <laurajorques2003@gmail.com>
2022-11-21 20:31:25 +0100

    Ventana barcos

--- VentanaMapa.java
+++ VentanaMapa.java
@@ -1,13 +1,24 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class VentanaMapa extends JFrame{
-    private JPanel mapa;
+    private final int COLS = 20;
+    private final int ROWS = 20;
+    private JPanel panel;
+    private JPanel panelBarcos;
+    private JComboBox comboBox1;
+
     public VentanaMapa() {
         super("BATTLESHIP");
-        setContentPane(mapa );
-        setSize(850,1050);
+        setContentPane(panel);
+        setSize(800,800);
+        for (int i =0; i<(COLS*ROWS); i++) {
+            JLabel label = new JLabel("");
+            label.setBorder(new LineBorder(Color.black));
+            label.setHorizontalAlignment(SwingConstants.CENTER);
+            panel.add(label);
+        }
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
