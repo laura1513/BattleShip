12695390a5c4c7e15b783afa848c4ed139733777
laura1513 <laurajorques2003@gmail.com>
2022-11-28 20:30:31 +0100

    Ventana ayuda

--- Info.java
+++ Info.java
@@ -1,20 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Info extends JFrame{
-    private JLabel paso1;
+    private JPanel info;
     private JLabel titulo;
+    private JLabel paso1;
     private JLabel paso2;
+    private JLabel aircraft;
+    private JLabel battle;
     private JLabel paso3;
+    private JLabel submarine;
     private JLabel paso4;
-    private JPanel panel;
+    private JLabel cruiser;
+    private JLabel paso5;
+    private JLabel destroyer;
 
-    public Info() {
-        super("");
-        setContentPane(panel);
-        setSize(900,500);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public  Info() {
+        super("INFO");
+        setContentPane(info);
+        setSize(700,600);
+        aircraft.setIcon(new ImageIcon("aircraft.png"));
+        battle.setIcon(new ImageIcon(("battleship.png")));
+        submarine.setIcon(new ImageIcon("submarine.png"));
+        cruiser.setIcon(new ImageIcon("cruiser.png"));
+        destroyer.setIcon(new ImageIcon("destroyer.png"));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
-
     }
 }
